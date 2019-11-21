e6b1cac72d1300b9b4469c9d777604ba2b46cb38
Nilay Barde <nilaybarde@gmail.com>
2019-11-21 18:12:26 -0500

    Typo

--- newsbee/assets/js/navbar.jsx
+++ newsbee/assets/js/navbar.jsx
@@ -5,7 +5,7 @@ import {NavLink} from 'react-router-dom'
 export default function Navigation(props) {
     return(
         <Navbar bg="primary" variant="dark" expand="lg">
-            <Navbar.Brand>Timesheets</Navbar.Brand>
+            <Navbar.Brand>Newsbee</Navbar.Brand>
             <Navbar.Toggle className="ml-auto" aria-controls="basic-navbar-nav"/>
             <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
                 <Nav className="ml-auto">
