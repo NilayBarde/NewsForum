41cf5890b12d0e641a61394ed65d14615d7b4c2e
jiahaozhuanshu <song.jiaha@husky.neu.edu>
2019-11-30 01:25:12 -0500

    fetch api in elixir

--- newsbee/mix.exs
+++ newsbee/mix.exs
@@ -43,7 +43,10 @@ defmodule Newsbee.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:argon2_elixir, "~> 2.0"}
+      {:argon2_elixir, "~> 2.0"},
+      {:httpoison, "~> 1.6"},
+      {:distillery, "~> 2.0"},
+      {:poison, "~> 3.1"}
     ]
   end
 
